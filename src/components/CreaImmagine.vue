c478bed3bbef3f0a2bfb8a51917112f9bf82e06a
Matteo Baccan <matteo.baccan@gmail.com>
2024-10-10 11:12:14 +0200

    Aggiunto info

--- src/components/CreaImmagine.vue
+++ src/components/CreaImmagine.vue
@@ -16,7 +16,7 @@
         <h1 class="text-2xl font-bold mb-5">Crea Immagine</h1>
         <div class="space-y-4">
           <div>
-            <label class="block text-sm font-medium text-gray-700">Contenuto sul quale generare una immagine</label>
+            <label class="block text-sm font-medium text-gray-700">Contenuto sul quale generare una immagine: al massimo 1000 caratteri - la lunghezza del prompt</label>
             <textarea v-model="post" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
               rows="4"></textarea>
           </div>
